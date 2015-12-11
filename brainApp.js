214b10fe6c5307f55f7b05f5fdbf3968bb41345b
Mees Boeijen <mees@amees.me>
2015-12-11 18:59:19 +0100

    Fixed bug and improved train logging and performance

--- brainApp.js
+++ brainApp.js
@@ -3,42 +3,46 @@ var fs = require('fs');
 var util = require('util');
 
 //Training settings
-var _samples = 60000;
+var _samples = 200;
 var _iterations = 50;
 var _error = 0.002;
 
 
 function convertTrainingData(data)
 {
-	var lines = data.toString().split('\n');
-	convertedData = [];
-	time = new Date();
-	for(var i = 0; i < lines.length; i++) {
-		var input = lines[i].split(',').map(function(value) {
-			return parseInt(value) / 255;
-		});
-		var output = Array.apply(null, Array(10)).map(Number.prototype.valueOf, 0);
-		output[input.shift() * 255] = 1;
-		convertedData.push({
-			input: input,
-			output: output
-		});
-	}
-	time = new Date() - time;
-	console.log("Converting data: " + time);
-	return convertedData;
+    var lines = data.toString().split('\n');
+    console.log('[CONVERTER] Converting '+ lines.length +' samples...');
+    convertedData = [];
+    var time = new Date();
+    for(var i = 0; i < lines.length; i++) {
+        var input = lines[i].split(',');
+
+        var actualInput = [];
+        for (var j = 0; j < input.length; j++) {
+            actualInput.push(parseInt(input[j]) / 255);
+        };
+        
+        var output = Array.apply(null, Array(10)).map(Number.prototype.valueOf, 0);
+        output[actualInput.shift() * 255] = 1;
+        convertedData.push({
+            input: actualInput,
+            output: output
+        });
+    }
+    time = new Date() - time;
+    console.log('[CONVERTER] Finished converting in '+ time +'ms.');
+    return convertedData;
 }
 
 fs.readFile(__dirname + '/data/train.csv', function (err, trainContent) {
     if (err) {
-        console.log('Error:', err);
+        console.log('[TRAINER] Failed:', err);
+        return;
     }
 
-    console.log(util.inspect(process.memoryUsage()));
     var trainData = convertTrainingData(trainContent);
-    var trainSample = trainData.slice(0, samples);
-    console.log(util.inspect(process.memoryUsage()));
-    console.log('Got ' + trainData.length + ' samples');
+    var trainSample = trainData.slice(0, _samples);
+    console.log('[TRAINER] Training with ' + _samples + ' samples...');
     var myICR = new brain.NeuralNetwork({
     	hiddenLayers: [392, 196]
     })
@@ -47,24 +51,25 @@ fs.readFile(__dirname + '/data/train.csv', function (err, trainContent) {
     myICR.train(trainSample, {
     	errorThresh: _error,
     	iterations: _iterations,
-    	log: true,
-    	logPeriod: 1,
-    	learningRate: 0.2
+        callbackPeriod: 1,
+    	learningRate: 0.2,
+        callback: function(result){
+            console.log('[TRAINER] Iteration '+result.iterations+' finished. Error-rate is '+result.error+'.');
+        }
     });
 
     var trainingTime = new Date() - time;
-    console.log('Training time: ' + trainingTime);
+    console.log('[TRAINER] Finished training in '+ trainingTime +'ms.');
     saveNetwork(myICR);
-
 });
 
 function saveNetwork(network) {
 	var networkJson = network.toJSON();
-    fs.writeFile("networks/brain_test.json", JSON.stringify(networkJson, null, 4), function(err) {
+    fs.writeFile('networks/brain_test.json', JSON.stringify(networkJson, null, 4), function(err) {
     	if(err) {
-    		return console.log("Writing error: " + err);
-    	}
-
-    	console.log("The file was saved!");
+    		return console.log('[SAVE] Failed: ' + err);
+    	}else{
+            console.log('[SAVE] The file was saved!');
+        }
     });
 }
